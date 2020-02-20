09b2e215b324771152283b1202f78e6d297e0123
Lukasz Wojtach <lukasz.wojtach@allegro.pl>
2020-02-20 23:53:05 +0100

    Do not hold state in a Note directly

--- app/src/main/java/pl/wojtach/silvarerum/alternative/Notes.kt
+++ app/src/main/java/pl/wojtach/silvarerum/alternative/Notes.kt
@@ -3,7 +3,6 @@ package pl.wojtach.silvarerum.alternative
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import pl.wojtach.silvarerum.room.NoteData
 import pl.wojtach.silvarerum.room.NotesDao
 
@@ -12,17 +11,15 @@ class Note(
     private val dao: NotesDao
 ) {
 
-    private var currentState: NoteData? = null
-
-    val data: Flow<NoteData> = dao.observeNote(id).distinctUntilChanged().onEach { currentState = it }
+    val data: Flow<NoteData> = dao.observeNote(id).distinctUntilChanged()
 
     suspend fun update(newContent: String) {
-        val newVersion = currentState?.copy(content = newContent) ?: NoteData(id = id, content = newContent)
+        val newVersion = NoteData(id = id, content = newContent)
         dao.update(newVersion)
     }
 
     suspend fun delete() {
-        val toDelete = currentState ?: NoteData(id, "")
+        val toDelete = NoteData(id, "")
         dao.delete(toDelete)
     }
 }
